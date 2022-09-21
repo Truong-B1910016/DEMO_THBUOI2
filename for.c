d7e0aba2f11ba0a6799f378dc57de18aead3aeb6
Nvt_B1910016 <truongB1910016@student.ctu.edu.vn>
2022-09-21 17:43:53 +0700

    added for.c

---
+++ for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int i, start, end;
+    start = 1;
+    end = 10;
+    printf("Cac so theo thu tu tang dan:\n");
+    for(i=start; i<=end; i++)
+       printf("%2d\n", i);
+       return 0;
+}
